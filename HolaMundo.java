73613177f911730d5b531059f6d45e9a72d8161b
Alejandro Medel Martínez <amm00331@red.ujaen.es>
2025-11-20 18:49:32 +0100

    Comentarios añadidos al codigo

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,10 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
- */
-
 /**
- *
+ * Muestra por pantalla la frase "¡Hola, Mundo!"
  * @author alexv
  */
 public class HolaMundo {
@@ -13,6 +8,6 @@ public class HolaMundo {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        // TODO code application logic here
+        System.out.println("¡Hola, Mundo!");
     }
 }
